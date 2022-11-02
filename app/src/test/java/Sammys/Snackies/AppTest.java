be465775b20b1761a55e46cc0ceead09cfbdb346
Brad Wootton <bwoo6196@uni.sydney.edu.ad>
2022-11-02 13:38:17 +1100

    a few more basic addProduct tests

--- app/src/test/java/Sammys/Snackies/AppTest.java
+++ app/src/test/java/Sammys/Snackies/AppTest.java
@@ -8,7 +8,6 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 
 class AppTest {
     @Test void appHasAGreeting() {
@@ -101,16 +100,42 @@ class AppTest {
     }
 
     @Test void addProductPositiveTest1() {
-        ArrayList<String> inputs = new ArrayList<>(Arrays.asList("productadd", "Z1", "ZooperDooper", "$2.00", "candy", "5"));
+        String[] s = {"productadd", "Z1", "ZooperDooper", "$2.00", "candy", "5"};
+        ArrayList<String> inputs = new ArrayList<>(Arrays.asList(s));
+        VendingMachine vm = new VendingMachine();
+        boolean result = App.addProduct(inputs, vm);
+        assertTrue(result);
+    }
+
+    @Test void addProductPositiveTest2() {
+        String[] s = {"productadd", "W1", "WagonWheels", "$1.00", "candy", "1"};
+        ArrayList<String> inputs = new ArrayList<>(Arrays.asList(s));
         VendingMachine vm = new VendingMachine();
         boolean result = App.addProduct(inputs, vm);
         assertTrue(result);
     }
 
     @Test void addProductNegativeTest1() {
-        ArrayList<String> inputs = new ArrayList<>(Arrays.asList("productadd", "Z1", "ZooperDooper", "-2.00", "candy", "5"));
+        String[] s = {"productadd", "Z1", "ZooperDooper", "-2.00", "candy", "5"};
+        ArrayList<String> inputs = new ArrayList<>(Arrays.asList(s));
         VendingMachine vm = new VendingMachine();
         boolean result = App.addProduct((ArrayList<String>)inputs, vm);
+        assertFalse(result);
+    }
+    
+    @Test void addProductNegativeTest2() {
+        String[] s = {"productadd", "W1", "WagonWheels", "1", "candy", "-1"};
+        ArrayList<String> inputs = new ArrayList<>(Arrays.asList(s));
+        VendingMachine vm = new VendingMachine();
+        boolean result = App.addProduct(inputs, vm);
+        assertFalse(result);
+    }
+
+    @Test void addProductNegativeTest3() {
+        String[] s = {"productadd", "W1", "WagonWheels", "1", "candy", "0"};
+        ArrayList<String> inputs = new ArrayList<>(Arrays.asList(s));
+        VendingMachine vm = new VendingMachine();
+        boolean result = App.addProduct(inputs, vm);
         assertFalse(result);
     }
 }
